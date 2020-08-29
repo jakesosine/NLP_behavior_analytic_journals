65486bfa6ae056e20868f8fdbfde9e83caa34efb
jakesosine <jacobsosine@gmail.com>
2020-08-29 12:16:59 -0700

    Refactored to bare essentials. Next step will be to set up with function

--- Ocr.py
+++ Ocr.py
@@ -1,56 +1,44 @@
 import cv2
-#import requests
+import requests
 import io
 import json
 import os
-import PyPDF2
+# API found free at https://ocr.space/
+# No affiliation to ocr.space
 
-for folderName, subfolders, filenames in os.walk('articles/'):
-    print(f'The folder is {folderName})
-    print(f'The subfolders in {folderName} are {subfolders}')
-    print(f'The subfolders in {folderName} are {filenames}')
-
-    for file in filenames:
-        if file.endswith('.png'):
-            pdfFile = open(file, 'rb')
-            reader = PyPDF2.PdfFileReader(pdfFile)
+api_value = 'key'  # Enter your own API Key
 
 
-#            img = cv2.imread(file)
- #           cv2.imshow('img', img)
-  #          cv2.waitKey(0)
-   #         cv2.destroyAllWindows()
+img = cv2.imread('screenshot2.png')
 
-
-#img = cv2.imread('screenshot2.png')
-
-#height, width, _ = img.shape
+# defining image height and width by using img.shape
+height, width, _ = img.shape
 
 # Cutting image
-#roi = img[0:height, 400:width]
+roi = img[0:height, 400:width]
 
-# ocr
-#url_api = 'https://api.ocr.space/parse/image'
+# OCR API
+url_api = 'https://api.ocr.space/parse/image'
+# Outlining the compressed image and then converting it to bytes using IO package
+_, compressedimage = cv2.imencode('.jpg', img, [1, 90])
+file_bytes = io.BytesIO(compressedimage)
 
-#_, compressedimage = cv2.imencode('.jpg', img, [1, 90])
-#file_bytes = io.BytesIO(compressedimage)
+# Sending post request to API
+results = requests.post(url_api,
+                        files={'screenshot.png': file_bytes},
+                        data={'apikey': api_Value})
 
 
-# results = requests.post(url_api,
-#                        files={'screenshot.png': file_bytes},
-#                        data={'apikey': 'api_Value'})
+result = results.content.decode()
+result = json.loads(result)
 
+parsed_results = result.get('ParsedResults')[0]
+text_detected = parsed_results.get('ParsedText')
 
-#result = results.content.decode()
-#result = json.loads(result)
+with open('data1.txt', 'a+') as file:
+    file.write(text_detected)
 
-#parsed_results = result.get('ParsedResults')[0]
-#text_detected = parsed_results.get('ParsedText')
-
-# with open('data1.txt', 'a+') as file:
-#    file.write(text_detected)
-
-#cv2.imshow('roi', roi)
-#cv2.imshow('img', img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imshow('roi', roi)
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
