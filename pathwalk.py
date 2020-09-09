77711e7c2eb115ce51d24eeb07f86f78d4d63ba9
jakesosine <jacobsosine@gmail.com>
2020-09-09 16:57:15 -0700

    no clue

--- pathwalk.py
+++ pathwalk.py
@@ -1,12 +1,12 @@
-import cv2
 from PIL import Image
 import pytesseract
 from pdf2image import convert_from_path
 import os
 
+# pytesseract location in homebrew.
 pytesseract.pytesseract.tesseract_cmd = r'/usr/local/Cellar/tesseract/4.1.1/bin/tesseract'
 
-
+# Optical Character Recognition Function
 def image_ocr(image_path, output_txt_file_name, All_text):
     image_text = pytesseract.image_to_string(
         image_path, lang='eng+ces', config='--psm 1')
@@ -16,19 +16,24 @@ def image_ocr(image_path, output_txt_file_name, All_text):
         f.write(image_text)
 
 
-num = 0
-year = 1970
+num = 0 # Number for Jpeg File
+year = 1974 # Year being recorded for cumulative text
 
+
+#Path to file for year being recorded
 year_being_recorded = 'txt_files/' + str(year) + '_article.txt'
+#Path to file for cumulative text
 cumulative_text = 'txt_files/cumulative.txt'
 
+
+#Pathwalk through the directory and retrive all folders, subfulders, and fnames in directory articles
 for foldername, subfolders, filenames in os.walk('articles'):
-    for file in filenames:
-        if file.endswith('.pdf'):
-            article_path = str(foldername) + '/' + str(file)
-            pages = convert_from_path(article_path, 500)
-            for page in pages:
-                name = 'jpegs/a_file_' + str(num) + '.jpeg'
-                page.save(name, 'JPEG')
-                image_ocr(name, year_being_recorded, cumulative_text)
-                num = num + 1
+    for file in filenames: # Only considers filenames
+        if file.endswith('.pdf'): # outlines that it should end with PDF
+            article_path = str(foldername) + '/' + str(file) #Joins article path
+            pages = convert_from_path(article_path, 500) #Converts pdf to Python image library format (known as pillow)
+            for page in pages: # Loop through each page
+                name = 'jpegs/a_file_' + str(num) + '.jpeg' # provide name for saving
+                page.save(name, 'JPEG') # save file for records
+                image_ocr(name, year_being_recorded, cumulative_text) #Optical character recognition function
+                num = num + 1 #add 1 for JPEG filename
