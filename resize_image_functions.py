3bd1063a8936c91e0b6d7801cfa238ab0625fdb3
jakesosine <jacobsosine@gmail.com>
2020-09-22 12:35:28 -0700

    working on image augmentation for for extracting all relevant details

--- resize_image_functions.py
+++ resize_image_functions.py
@@ -77,12 +77,7 @@ def border_crop_resize(image_path, scale_percentage):
 
 
 def get_grayscale(image_path):
-    image = cv2.imread(image_path)
-    cv2.imshow('original_image', image)
-    gray_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('gray_scale_image', gray_scale)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    return cv2.cvtColor(image_path, cv2.COLOR_BGR2GRAY)
 
 
 def canny_edge_detection(image_path):
@@ -94,13 +89,8 @@ def canny_edge_detection(image_path):
     cv2.destroyAllWindows()
 
 
-def median_Blur(image_path):
-    image = cv2.imread(image_path)
-    cv2.imshow('original_image', image)
-    median_Blur = cv2.medianBlur(image, 5)
-    cv2.imshow('medianBlur', median_Blur)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+def medianblur(image_path):
+    return cv2.medianBlur(image_path, 5)
 
 
 def thresh_bin(image_path):
@@ -111,6 +101,7 @@ def thresh_bin(image_path):
     cv2.imshow('thresh_bin', thresh)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    return thresh
 
 # Takes a while
 
@@ -127,19 +118,21 @@ def bounding_boxes_letters(image_path):
 
 
 def bounding_boxes_to_words(image_path):
+
     img = cv2.imread(image_path)
     d = pytesseract.image_to_data(img, output_type=Output.DICT)
     n_boxes = len(d['text'])
     for i in range(n_boxes):
         if int(d['conf'][i]) > 60:
-            (x, y, w, h) = (d['left'][i], d['top']
-                            [i], d['width'][i], d['height'][i])
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            (x, y, w, h) = (d['left'][i], d['top'][i],
+                            d['width'][i], d['height'][i])
+            img = cv2.rectangle(
+                img, (x, y), (x + w, y + h), (255, 255, 0), 2)
     return img
 
 
-img = bounding_boxes_to_words('jpegs/a_file_1278.jpeg')
-cv2.imshow('img_bounding_boxes_on_words', img)
+canny = get_grayscale('jpegs/a_file_0.jpeg')
+cv2.imshow('img_bounding_boxes_on_words', canny)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 #n_boxes = len(d['text'])
