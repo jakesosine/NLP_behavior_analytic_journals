65a0eac9cc675c5a878d57ace6e3183a02f54308
jakesosine <jacobsosine@gmail.com>
2020-09-22 21:29:46 -0700

    added preprocessing functions for identifying methods, references, results

--- preprocessing_text.py
+++ preprocessing_text.py
@@ -1,5 +1,5 @@
 import re
-
+import pandas as pd
 # passes an open text file through this function with a start and end string to return what is between those two values.
 
 
@@ -23,6 +23,27 @@ def keywords_text(opened_file, start_identifier, end_identifier):
     return opened_file[start:end]
 
 
+def clean_text(text, start_identifier):
+    text = text.replace(start_identifier, "")
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"how's", "how is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"n't", " not", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", text)
+    return text
+
+
 test_string = '''Key words: ambulation responses, microswitches, multiple disabilities, indices of happiness,
 contingency, quality of life, social validation
 
@@ -31,13 +52,16 @@ mental, disabilities
 sometimes present with gait and posture'''
 
 
-print(keywords_text(test_string, 'Key words:', 'posture'))
+#text = keywords_text(test_string, 'Key words:', 'posture')
+#print(clean_text(text, "Key words:"))
 
 
 # Open file With context manager
 with open('txt_files/article0.txt', 'r') as f:
     f_contents = f.read()
-    print(keywords_text(f_contents, 'Key words', 'METHOD'))
-    print(text_gathering(f_contents, "METHOD", "RESULTS"))
-    print(text_gathering(f_contents, "RESULTS", "REFERENCES"))
-    print(text_references(f_contents, "REFERENCES"))
+    #print(keywords_text(f_contents, 'Key words', 'METHOD'))
+    method = (text_gathering(f_contents, "METHOD", "RESULTS"))
+    method = clean_text(method, "METHOD")
+    print(method)
+    #print(text_gathering(f_contents, "RESULTS", "REFERENCES"))
+    #print(text_references(f_contents, "REFERENCES"))
