91e13abd56474f2c40107034529b6af6685da9ee
jakesosine <jacobsosine@gmail.com>
2020-09-23 17:39:11 -0700

    this will eventually take the place of the OCR_Article_to_Text.py file due to added cropping

--- test.py
+++ test.py
@@ -6,11 +6,29 @@ import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r'/usr/local/Cellar/tesseract/4.1.1/bin/tesseract'
 
-y = 350
+y = 200
 h = 5000
 x = 0
 w = 3440
-scale_percentage = 20
+
+y1 = 150
+h1 = 5000
+x1 = 0
+w1 = 3440
+
+
+def crop_page_1(image):
+    image = cv2.imread(image)
+    img = image[y1:y1 + image.shape[1],
+                x1:x1 + image.shape[0]]  # Numpy Slicing
+    return img
+
+
+def crop_page_2_through_end(image):
+    img = cv2.imread(image)
+    crop_img = img[y:y + h,
+                   x:x + w]  # Numpy Slicing
+    return crop_img
 
 
 def image_ocr(image_path, output_txt_file_name):
@@ -20,31 +38,41 @@ def image_ocr(image_path, output_txt_file_name):
         f.write(image_text)
 
 
-y1 = 230
-h1 = 5000
-x1 = 0
-w1 = 3440
+article_number = 0
+saved_image_num = 0
+text_file = 'txt_files/' + 'article'
 
 
-def crop_page_1(image_path):
-    img = cv2.imread(image_path)
-    img = cv2.medianBlur(img, 5)
-    cv2.imshow('canny_edge_detection', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    crop_img = img[y1:y1 + img.shape[1], x1:x1 + img.shape[0]]  # Numpy Slicing
-    return crop_img
+for root, dirs, files in os.walk('articles'):
+    for file_ in files:
+        if file_.endswith('.pdf'):
+            article_path = str(root) + '/' + str(file_)
+            pages = convert_from_path(article_path, dpi=300)
+            length_of_article = len(pages)
+            print(length_of_article)
+            page_number = 0
+            for page in pages:
+                if page_number == 0:
+                    print('in if condition')
+                    name = 'jpegs/file_' + str(saved_image_num) + '.jpeg'
+                    page.save(name, 'JPEG')
+                    img = crop_page_1(name)
+                    page_number += 1
+                    saved_image_num += 1
+                    image_ocr(img, text_file + str(article_number) + '.txt')
 
+                else:
+                    print('in elif condition')
+                    name_ = 'jpegs/file_' + str(saved_image_num) + '.jpeg'
+                    page.save(name_, 'JPEG')
+                    img1 = crop_page_2_through_end(name_)
+                    saved_image_num += 1
+                    page_number += 1
+                    image_ocr(img1, text_file + str(article_number) + '.txt')
 
-img = (crop_page_1('jpegs/a_file_11.jpeg'))
-
-
-def crop_and_resize_page_2_through_end(image_path):
-    img = cv2.imread(image_path)
-    print('Original Dimensions : ', img.shape)
-    crop_img = img[y:y + img.shape[1], x:x + img.shape[0]]  # Numpy Slicing
-    return crop_img
-
-
-#img = crop_and_resize('jpegs/a_file_5.jpeg')
-image_ocr(img, 'txt_files/' + 'article_test' + '.txt')
+                    if page_number == length_of_article:
+                        print('in else condition')
+                        article_number += 1
+                        print(page_number)
+                        page_number = page_number - length_of_article
+                        print(page_number)
